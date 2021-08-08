58672cda5e8de4a9f3d3531875a48725b13ff58e
SolidCoffee <88467179+SolidCoffee@users.noreply.github.com>
2021-08-07 21:50:23 -0400

    Update stm32f407xx.h

--- stm32f4xx/stm32f407xx.h
+++ stm32f4xx/stm32f407xx.h
@@ -75,6 +75,7 @@ typedef struct
 	__vo uint32_t MODER;
 	__vo uint32_t OTYPER;
 	__vo uint32_t OSPEEDR;
+	__vo uint32_t PUPDR;
 	__vo uint32_t IDR;
 	__vo uint32_t ODR;
 	__vo uint32_t BSRR;
@@ -133,5 +134,62 @@ typedef struct
 }RCC_RegDef_t;
 
 #define RCC					((RCC_RegDef_t*)RCC_BASE)
+
+
+//enable clock for GPIOx peripherals
+#define GPIOA_PCLK_EN()	(RCC->AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN()	(RCC->AHB1ENR |= (1 << 1))
+#define GPIOC_PCLK_EN()	(RCC->AHB1ENR |= (1 << 2))
+#define GPIOD_PCLK_EN()	(RCC->AHB1ENR |= (1 << 3))
+#define GPIOE_PCLK_EN()	(RCC->AHB1ENR |= (1 << 4))
+#define GPIOF_PCLK_EN()	(RCC->AHB1ENR |= (1 << 5))
+#define GPIOG_PCLK_EN()	(RCC->AHB1ENR |= (1 << 6))
+#define GPIOH_PCLK_EN()	(RCC->AHB1ENR |= (1 << 7))
+#define GPIOI_PCLK_EN()	(RCC->AHB1ENR |= (1 << 8))
+
+
+//enable macros for I2Cx clock peripherals
+#define I2C1_PLCLK_EN()	(RCC->APB1ENR |= (1 << 21))
+
+
+//CLOCK ENABLE MACROS FOR spiX PERIPHERALS
+#define SPI1_PLCLK_EN()	(RCC->APB2ENR |= (1 << 12))
+
+//Clock disable macros for GPIOx peripherals
+#define GPIOA_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 4))
+#define GPIOF_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 5))
+#define GPIOG_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 6))
+#define GPIOH_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 7))
+#define GPIOI_PCLK_DI()	(RCC->AHB1ENR &= ~(1 << 8))
+
+
+
+//GPIO reset maco's
+#define GPIOA_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 0)); (RCC->AHB1RSTR &= ~(1 <<  0)); }while(0)
+#define GPIOB_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 1)); (RCC->AHB1RSTR &= ~(1 <<  1)); }while(0)
+#define GPIOC_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 2)); (RCC->AHB1RSTR &= ~(1 <<  2)); }while(0)
+#define GPIOD_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 3)); (RCC->AHB1RSTR &= ~(1 <<  3)); }while(0)
+#define GPIOE_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 4)); (RCC->AHB1RSTR &= ~(1 <<  4)); }while(0)
+#define GPIOF_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 5)); (RCC->AHB1RSTR &= ~(1 <<  5)); }while(0)
+#define GPIOG_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 6)); (RCC->AHB1RSTR &= ~(1 <<  6)); }while(0)
+#define GPIOH_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 7)); (RCC->AHB1RSTR &= ~(1 <<  7)); }while(0)
+#define GPIOI_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 8)); (RCC->AHB1RSTR &= ~(1 <<  8)); }while(0)
+
+
+
+
+#define ENABLE			1
+#define DISABLE			0
+#define SET				ENABLE
+#define RESET			DISABLE
+
+#define INPUT			0
+#define OUTPUT			1
+#define ALTFUNC			2
+#define ANALOG			3
 
 #endif /* INC_STM32F407XX_H_ */
