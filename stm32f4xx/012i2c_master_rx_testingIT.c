07ccb727aece3ee0dfb4a7553703200eb438c55a
SolidCoffee <88467179+SolidCoffee@users.noreply.github.com>
2021-08-28 23:48:35 -0400

    Create 012i2c_master_rx_testingIT.c

---
+++ stm32f4xx/012i2c_master_rx_testingIT.c
@@ -0,0 +1,186 @@
+#include "stm32f407xx.h"
+#include "stm32f407xx_gpio_driver.h"
+#include "stm32f407xx_spi_driver.h"
+#include "stm32f407xx_I2C_driver.h"
+#include<stdio.h>
+#include<string.h>
+
+
+extern void initialise_monitor_handles();
+
+//Flag variable
+uint8_t rxComplt = RESET;
+
+#define MY_ADDR 0x61;
+
+#define SLAVE_ADDR  0x68
+
+void delay(void)
+{
+	for(uint32_t i = 0 ; i < 500000/2 ; i ++);
+}
+
+I2C_Handle_t I2C1Handle;
+
+//rcv buffer
+uint8_t rcv_buf[32];
+
+/*
+ * PB6-> SCL
+ * PB7 -> SDA
+ */
+
+void I2C1_GPIOInits(void)
+{
+	GPIO_Handler_t I2CPins;
+
+	I2CPins.pGPIOx = GPIOB;
+	I2CPins.GPIO_PinConfig.GPIO_PinMode = ALTFUNC;
+	I2CPins.GPIO_PinConfig.GPIO_PinOPType = OPENDRAIN;
+	I2CPins.GPIO_PinConfig.GPIO_pinPuPdControl = PULLUP;
+	I2CPins.GPIO_PinConfig.GPIO_PinAltFunMode = 4;
+	I2CPins.GPIO_PinConfig.GPIO_PinSpeed = FAST;
+
+	//scl
+	I2CPins.GPIO_PinConfig.GPIO_PinNumber = 6;
+	GPIO_Init(&I2CPins);
+
+
+	//sda
+	I2CPins.GPIO_PinConfig.GPIO_PinNumber = 7;
+	GPIO_Init(&I2CPins);
+
+
+}
+
+void I2C1_Inits(void)
+{
+	I2C1Handle.pI2Cx = I2C1;
+	I2C1Handle.I2C_Config.I2C_ACKControl = ENABLE;
+	I2C1Handle.I2C_Config.I2C_DeviceAddress = MY_ADDR;
+	I2C1Handle.I2C_Config.I2C_FMDutyCycle = DUTY2;
+	I2C1Handle.I2C_Config.I2C_SCLSpeed = I2C_SM;
+
+	I2C_init(&I2C1Handle);
+
+}
+
+void GPIO_ButtonInit(void)
+{
+	GPIO_Handler_t GPIOBtn;
+
+	GPIOBtn.pGPIOx = GPIOA;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = 0;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = INPUT;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_pinPuPdControl =NOPUPD;
+
+	GPIO_Init(&GPIOBtn);
+
+}
+
+
+int main(void)
+{
+
+	uint8_t commandcode;
+
+	uint8_t len;
+
+	initialise_monitor_handles();
+
+	printf("Application is running\n");
+
+	GPIO_ButtonInit();
+
+	//i2c pin inits
+	I2C1_GPIOInits();
+
+	//i2c peripheral configuration
+	I2C1_Inits();
+
+	//I2C IRQ configurations
+	I2C_IRQInterruptConfig(IRQ_I2C1_EV,ENABLE);
+	I2C_IRQInterruptConfig(IRQ_I2C1_ER,ENABLE);
+
+	//enable the i2c peripheral
+	I2C_PeripheralControl(I2C1,ENABLE);
+
+	//ack bit is made 1 after PE=1
+	I2C_ManageAcking(I2C1,ENABLE);
+
+	while(1)
+	{
+		//wait till button is pressed
+		while( ! GPIO_ReadFromInputPin(GPIOA,0) );
+
+		//to avoid button de-bouncing related issues 200ms of delay
+		delay();
+
+		commandcode = 0x51;
+
+
+		while(I2C_MasterSendDataIT(&I2C1Handle,&commandcode,1,SLAVE_ADDR,I2C_ENABLE_SR) != READY);
+
+		while(I2C_MasterReceiveDataIT(&I2C1Handle,&len,1,SLAVE_ADDR,I2C_ENABLE_SR)!= READY);
+
+
+
+		commandcode = 0x52;
+		while(I2C_MasterSendDataIT(&I2C1Handle,&commandcode,1,SLAVE_ADDR,I2C_ENABLE_SR) != READY);
+
+
+		while(I2C_MasterReceiveDataIT(&I2C1Handle,rcv_buf,len,SLAVE_ADDR,I2C_DISABLE_SR)!= READY);
+
+		rxComplt = RESET;
+
+		//wait till rx completes
+        while(rxComplt != SET);
+
+		rcv_buf[len+1] = '\0';
+
+		printf("Data : %s",rcv_buf);
+
+		rxComplt = RESET;
+
+	}
+
+}
+
+
+void I2C1_EV_IRQHandler (void)
+{
+	I2C_EV_IRQHandling(&I2C1Handle);
+}
+
+
+void I2C1_ER_IRQHandler (void)
+{
+	I2C_ER_IRQHandling(&I2C1Handle);
+}
+
+
+
+void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle,uint8_t AppEv)
+{
+     if(AppEv == I2C_EV_TX_CMPLT)
+     {
+    	 printf("Tx is completed\n");
+     }else if (AppEv == I2C_EV_RX_CMPLT)
+     {
+    	 printf("Rx is completed\n");
+    	 rxComplt = SET;
+     }else if (AppEv == I2C_ERROR_AF)
+     {
+    	 printf("Error : Ack failure\n");
+    	 //in master ack failure happens when slave fails to send ack for the byte
+    	 //sent from the master.
+    	 I2C_CloseSendData(pI2CHandle);
+
+    	 //generate the stop condition to release the bus
+    	 I2C_GenerateStopCondition(I2C1);
+
+    	 //Hang in infinite loop
+    	 while(1);
+     }
+}
