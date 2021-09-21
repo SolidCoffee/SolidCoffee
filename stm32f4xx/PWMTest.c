0fc2605e2dd0fb8cadbfff5b4d38a3fa4cb4d0a3
Gianluca Mazzi <88467179+SolidCoffee@users.noreply.github.com>
2021-09-20 20:26:41 -0400

    Create PWMTest.c
    Test of PWM features and multiple channel usage along with full range of motion check on servo motor and stepping accuracy

---
+++ stm32f4xx/PWMTest.c
@@ -0,0 +1,84 @@
+#include "stm32f407xx.h"
+#include "stm32f407xx_gpio_driver.h"
+#include "stm32f407xx_timer.h"
+#include<stdio.h>
+#include<string.h>
+
+void delay2(void)
+{
+	for(uint32_t i = 0 ; i < 10000000 ; i ++);
+}
+
+void delay(void)
+{
+	for(uint32_t i = 0 ; i < 100000/2; i ++);
+}
+
+
+void PWM_GPIOInits(void)
+{
+	GPIO_Handler_t PWMPins;
+
+	PWMPins.pGPIOx = GPIOB;
+
+	PWMPins.GPIO_PinConfig.GPIO_PinMode = ALTFUNC;
+	PWMPins.GPIO_PinConfig.GPIO_PinAltFunMode = AF2;
+	PWMPins.GPIO_PinConfig.GPIO_PinOPType = PUSHPULL;
+	PWMPins.GPIO_PinConfig.GPIO_pinPuPdControl = NOPUPD;
+	PWMPins.GPIO_PinConfig.GPIO_PinSpeed = SPEEDHIGH;  //might need to look into this
+
+	//PWM Pin config
+	PWMPins.GPIO_PinConfig.GPIO_PinNumber = 6;
+	GPIO_Init(&PWMPins);
+
+	PWMPins.GPIO_PinConfig.GPIO_PinNumber = 7;
+	GPIO_Init(&PWMPins);
+}
+
+void PWMTIM(void)
+{
+	TIM_Handler_t TIMPWM;
+
+	TIMPWM.pTIMx = TIM4;
+	TIMPWM.TIM_Config.TIM_Direction = UPCNT;
+	TIMPWM.TIM_Config.TIM_ARR = 479;
+	TIMPWM.TIM_Config.TIM_Prescaler = 99;
+	TIMPWM.TIM_Config.TIM_Mode = EDGE;
+	TIMPWM.TIM_Config.PWM_Mode = PWM1;
+	TIMPWM.TIM_Config.TIM_DutyCycle = 479*.20;
+	TIMPWM.TIM_Config.TIM_Polarity = POLHIGH;
+	TIMPWM.TIM_Config.PWM_Channel = 1;
+
+	PWM2_5_Init(&TIMPWM);
+
+	TIMPWM.TIM_Config.PWM_Channel = 2;
+
+	PWM2_5_Init(&TIMPWM);
+}
+
+int main(void)
+{
+	PWM_GPIOInits();
+	TIM2_5_CLKEnable(TIM4, ENABLE); //Enables HSI and TIM3 peripheral clock
+
+	PWMTIM();
+
+	delay2();
+
+	while(1)
+	{
+		for(uint32_t i = 1; i < 270; i++)
+		{
+			ServoAngle(TIM4, i, 1);
+			ServoAngle(TIM4, i, 2);
+			delay();
+		}
+		for(uint32_t i = 270; i > 1; i--)
+		{
+			ServoAngle(TIM4, i, 1);
+			ServoAngle(TIM4, i, 2);
+			delay();
+		}
+	}
+
+}
