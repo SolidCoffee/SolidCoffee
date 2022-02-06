8bfc5b6077d9f3d605c57bda90eaa98b659a96a3
Gianluca Mazzi <88467179+SolidCoffee@users.noreply.github.com>
2022-02-05 23:21:12 -0500

    Update PWMTest.c

--- stm32f4xx/PWMTest.c
+++ stm32f4xx/PWMTest.c
@@ -1,6 +1,7 @@
 #include "stm32f407xx.h"
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_timer.h"
+#include "stm32f407xx_ADC_driver.h"
 #include<stdio.h>
 #include<string.h>
 
@@ -14,6 +15,110 @@ void delay(void)
 	for(uint32_t i = 0 ; i < 80000; i ++);
 }
 
+uint32_t x=10;
+uint32_t y=10;
+
+void ADC_Init_LR(ADC_RegDef_t *pADCHandle)
+{
+	//Enable ADC
+	pADCHandle->CR2 |= (1 << 0);
+
+	//continous conversion setting
+	pADCHandle->CR2 |= (1 << 1);
+
+	//clearing channel
+	pADCHandle->SQR3 &= ~(31 << 0);
+
+	//Selecting channel
+	pADCHandle->SQR3 |= (1 << 0);
+
+	//cycle edit
+	pADCHandle->SMPR2 |= (0 << 4);
+
+	//resolution
+	pADCHandle->CR1 |= (1 << 24);
+
+	//start conversion of regular channels
+	pADCHandle->CR2 |= (1 << 30);
+
+	if(pADCHandle->DR > 600)
+	{
+		x++;
+		ServoAngle(TIM4, x, 2);
+	}
+	else if(pADCHandle->DR < 500)
+	{
+		x--;
+		ServoAngle(TIM4, x, 2);
+	}
+	//Disable ADC
+	pADCHandle->CR2 &= ~(1 << 0);
+}
+
+void ADC_Init_UD(ADC_RegDef_t *pADCHandle)
+{
+	//Enable ADC
+	pADCHandle->CR2 |= (1 << 0);
+
+	//continous conversion setting
+	pADCHandle->CR2 |= (1 << 1);
+
+	//clearing channel
+	pADCHandle->SQR3 &= ~(31 << 0);
+
+	//Selecting channel
+	pADCHandle->SQR3 |= (3 << 0);
+
+	//cycle edit
+	pADCHandle->SMPR2 |= (0 << 4);
+
+	//resolution
+	pADCHandle->CR1 |= (1 << 24);
+
+	//start conversion of regular channels
+	pADCHandle->CR2 |= (1 << 30);
+
+	if(pADCHandle->DR > 600)
+	{
+		y++;
+		ServoAngle(TIM4, y, 1);
+	}
+	else if(pADCHandle->DR < 500)
+	{
+		y--;
+		ServoAngle(TIM4, y, 1);
+	}
+	//Disable ADC
+	pADCHandle->CR2 &= ~(1 << 0);
+}
+
+void GPIOInits(void)
+{
+	GPIO_Handler_t ADCPins;
+
+	ADCPins.pGPIOx = GPIOA;
+
+	ADCPins.GPIO_PinConfig.GPIO_PinMode = ANALOG;
+	ADCPins.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	ADCPins.GPIO_PinConfig.GPIO_PinOPType = PUSHPULL;
+	ADCPins.GPIO_PinConfig.GPIO_pinPuPdControl = NOPUPD;
+	ADCPins.GPIO_PinConfig.GPIO_PinSpeed = SPEEDHIGH;
+
+	//PWM Pin config
+	ADCPins.GPIO_PinConfig.GPIO_PinNumber = 1;
+	GPIO_Init(&ADCPins);
+
+	ADCPins.GPIO_PinConfig.GPIO_PinNumber = 3;
+	GPIO_Init(&ADCPins);
+
+	ADCPins.GPIO_PinConfig.GPIO_PinNumber = 5;
+	ADCPins.GPIO_PinConfig.GPIO_PinMode = OUTPUT;
+	GPIO_Init(&ADCPins);
+
+	ADCPins.GPIO_PinConfig.GPIO_PinNumber = 7;
+	ADCPins.GPIO_PinConfig.GPIO_PinMode = OUTPUT;
+	GPIO_Init(&ADCPins);
+}
 
 void PWM_GPIOInits(void)
 {
@@ -79,6 +184,8 @@ void GPIO_ButtonInit()
 
 int main(void)
 {
+	GPIOInits();
+	ADC_Clk_EnorDi(ADC1, ENABLE);
 	PWM_GPIOInits();
 	TIM2_5_CLKEnable(TIM4, ENABLE); //Enables HSI and TIM3 peripheral clock
 
@@ -87,13 +194,22 @@ int main(void)
 	PWMTIM();
 
 	//delay2();
+	while(1)
+	{
+		//ADC_Init_LR(ADC1);
+
+		///ADC_Init_UD(ADC1);
+
+		//ServoAngle(TIM4, 100, 3);
+		//ServoAngle(TIM4, 100, 1);
+		//ServoAngle(TIM4, 180, 2);
+	}
 
 
-	ServoAngle(TIM4, 170, 3);
-	ServoAngle(TIM4, 135, 1);
-	ServoAngle(TIM4, 135, 2);
-
-	/*while(1)
+	/*ServoAngle(TIM4, 100, 3);
+	ServoAngle(TIM4, 10, 1);
+	ServoAngle(TIM4, 180, 2);
+	while(1)
 		{
 			for(uint32_t i = 15; i < 180; i++)
 			{
@@ -113,7 +229,6 @@ int main(void)
 
 	/*while(1)
 	{
-
 		ServoAngle(TIM4, 1, 1);
 		while(! GPIO_ReadFromInputPin(GPIOA,0));
 		delay();
