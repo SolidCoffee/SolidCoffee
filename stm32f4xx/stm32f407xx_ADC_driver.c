eae1bdb61583eaf443cadea5eff85f89eef4de54
Gianluca Mazzi <88467179+SolidCoffee@users.noreply.github.com>
2022-02-25 22:08:40 -0500

    Update stm32f407xx_ADC_driver.c

--- stm32f4xx/stm32f407xx_ADC_driver.c
+++ stm32f4xx/stm32f407xx_ADC_driver.c
@@ -5,6 +5,11 @@ void delay2(void)
 	for(uint32_t i = 0 ; i < 5000 ; i ++);
 }
 
+void delay1(void)
+{
+	for(uint32_t i = 0 ; i < 10000 ; i ++);
+}
+
 void ADC_Clk_EnorDi(ADC_RegDef_t *pADCx, uint8_t EnorDi)
 {
 	if(EnorDi==ENABLE)
@@ -102,13 +107,23 @@ void ADC_Init_LR(ADC_RegDef_t *pADCHandle)
 
 	delay2();
 
-	if(pADCHandle->DR > 700)
+	if(pADCHandle->DR > 900)
 	{
 		LR_Inc_Flg=1;
 	}
-	else if(pADCHandle->DR < 400)
+	else if(pADCHandle->DR < 110)
 	{
 		LR_Dec_Flg=1;
+	}
+	else if((pADCHandle->DR < 400) && (pADCHandle->DR >= 200))
+	{
+		LR_Dec_Flg=1;
+		delay1();
+	}
+	else if((pADCHandle->DR > 700) && (pADCHandle->DR <= 900))
+	{
+		LR_Inc_Flg=1;
+		delay1();
 	}
 	//Disable ADC
 	pADCHandle->CR2 &= ~(1 << 0);
@@ -139,16 +154,80 @@ void ADC_Init_UD(ADC_RegDef_t *pADCHandle)
 
 	delay2();
 
-	if(pADCHandle->DR > 700)
+	if(pADCHandle->DR > 900)
 	{
 		UD_Inc_Flg=1;
 	}
-	else if(pADCHandle->DR < 400)
+	else if(pADCHandle->DR < 110)
 	{
 		UD_Dec_Flg=1;
 	}
+	else if((pADCHandle->DR < 400) && (pADCHandle->DR >= 200))
+	{
+		UD_Dec_Flg=1;
+		delay1();
+	}
+	else if((pADCHandle->DR > 700) && (pADCHandle->DR <= 900))
+	{
+		UD_Inc_Flg=1;
+		delay1();
+	}
 	//Disable ADC
 	pADCHandle->CR2 &= ~(1 << 0);
+}
+
+void ADC_Wrist(ADC_RegDef_t *pADCHandle)
+{
+	//Enable ADC
+	pADCHandle->CR2 |= (1 << 0);
+
+	//continous conversion setting
+	pADCHandle->CR2 |= (1 << 1);
+
+	//clearing channel
+	pADCHandle->SQR3 &= ~(31 << 0);
+
+	//Selecting channel
+	pADCHandle->SQR3 |= (5 << 0);
+
+	//cycle edit
+	pADCHandle->SMPR2 |= (0 << 4);
+
+	//resolution
+	pADCHandle->CR1 |= (1 << 24);
+
+	//start conversion of regular channels
+	pADCHandle->CR2 |= (1 << 30);
+
+	delay2();
+
+	if((pADCHandle->DR > 700) && (pADCHandle->DR <= 900))
+	{
+		Wrist_Inc_Flg=1;
+		delay1();
+	}
+	else if((pADCHandle->DR < 400) && (pADCHandle->DR >= 200))
+	{
+		Wrist_Dec_Flg=1;
+		delay1();
+	}
+	else if(pADCHandle->DR < 110)
+	{
+		Wrist_Dec_Flg=1;
+	}
+	else if(pADCHandle->DR > 900)
+	{
+		Wrist_Inc_Flg=1;
+	}
+	//Disable ADC
+	pADCHandle->CR2 &= ~(1 << 0);
+}
+
+uint32_t EdgeCondition(uint32_t val)
+{
+	if(val >= 180){val=180;}
+	else if(val <= 10){val=10;}
+	return val;
 }
 
 
