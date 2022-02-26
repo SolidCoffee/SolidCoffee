ac1f122156b9774adb968393d8998796e17bd967
Gianluca Mazzi <88467179+SolidCoffee@users.noreply.github.com>
2022-02-25 22:09:11 -0500

    Update stm32f407xx_ADC_driver.h

--- stm32f4xx/stm32f407xx_ADC_driver.h
+++ stm32f4xx/stm32f407xx_ADC_driver.h
@@ -1,3 +1,10 @@
+/*
+ * stm32f407xx_ADC_driver.h
+ *
+ *  Created on: Feb 5, 2022
+ *      Author: mmazzi
+ */
+
 #ifndef INC_STM32F407XX_ADC_DRIVER_H_
 #define INC_STM32F407XX_ADC_DRIVER_H_
 
@@ -5,6 +12,8 @@
 
 typedef struct
 {
+	uint32_t ADCChannel;
+	uint32_t ADCConversion;
 	uint8_t ADCResolution;
 	uint8_t ADCCycles;
 
@@ -26,11 +35,14 @@ typedef struct
 #define C144		6
 #define C480		7
 
+#define SINGLE		0
+#define CONT		1
+
 typedef struct
 {
 	//pointer to hold the base address for the GPIO peripheral
 	ADC_RegDef_t *pADCx;
-	ADC_Config_t ADC_Config_t;
+	ADC_Config_t ADC_Config;
 
 }ADC_Handler_t;
 
@@ -42,11 +54,15 @@ void ADC_Init(ADC_RegDef_t *pADCHandle);
 
 void ADC_Init_LR(ADC_RegDef_t *pADCHandle);
 void ADC_Init_UD(ADC_RegDef_t *pADCHandle);
+void ADC_Wrist(ADC_RegDef_t *pADCHandle);
+
+uint32_t EdgeCondition(uint32_t val);
 
 uint8_t LR_Inc_Flg;
 uint8_t UD_Inc_Flg;
 uint8_t LR_Dec_Flg;
 uint8_t UD_Dec_Flg;
-uint8_t Wrist_flg;
+uint8_t Wrist_Inc_Flg;
+uint8_t Wrist_Dec_Flg;
 
 #endif /* INC_STM32F407XX_ADC_DRIVER_H_ */
