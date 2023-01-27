469e821602b2538b6362c2dbfe04f7eabb1b2bb9
Gianluca Mazzi <88467179+SolidCoffee@users.noreply.github.com>
2023-01-26 23:11:22 -0500

    Update LCD.c

--- stm32f4xx/LCD.c
+++ stm32f4xx/LCD.c
@@ -7,29 +7,20 @@
 
 
 #include "stm32f407xx.h"
-void LCD_timer_init(void)
-{
-	BasicTIM_Handler_t LCD_timer;
-
-	LCD_timer.pTIMx = TIM6;
-	LCD_timer.TIM_Config.Buffer=0;
-	LCD_timer.TIM_Config.Interrupt=0;
-	LCD_timer.TIM_Config.MMS=2;
-	LCD_timer.TIM_Config.Mode=0;
-	LCD_timer.TIM_Config.Prescaler=1600-1;
-
-	BASIC_TIMCLKEnable(TIM6, ENABLE);
-
-	BasicTIM_INIT(&LCD_timer);
-}
 
 
 
 int main(void)
 {
+	lcd_init();
 
+	char str[10];
 
+	uint8_t num = 39;
 
+	sprintf(str, "%d", num);
+
+	lcd_print_string(str);
 
 	while(1)
 	{
